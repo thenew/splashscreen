ae55a37847b525c01e8396872660de1470dbac80
Rémy <remy@colorz.fr>
2012-11-20 18:13:09 +0100

    init

---
+++ inc/tpl/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr-FR" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <title>title</title>
+    <link rel="stylesheet" href="assets/css/zz-all.css" />
+    <script src="assets/js/mootools-yui-compressed.js"></script>
+    <script src="assets/js/events.js"></script>
+    <!--[if lt IE 9]>
+        <script src="assets/js/html5.js"></script>
+        <script src="assets/js/selectivizr-min.js"></script>
+    <![endif]-->
+</head>
+<!--[if lt IE 7 ]><body is_ie6 lt_ie7 lt_ie8 lt_ie9 lt_ie10'); ?>><![endif]-->
+   <!--[if IE 7 ]><body is_ie7 lt_ie8 lt_ie9 lt_ie10'); ?>><![endif]-->
+   <!--[if IE 8 ]><body is_ie8 lt_ie9 lt_ie10'); ?>><![endif]-->
+   <!--[if IE 9 ]><body is_ie9 lt_ie10'); ?>><![endif]-->
+<!--[if gt IE 9 ]><body is_ie10 gt_ie9'); ?>><![endif]-->
+<!--[if !IE]><!--><body><!--<![endif]-->
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include dirname(__FILE__).'/inc/tpl/header.php';
+?>
+<div id="" class="">
+
+</div>
+<?php
+include dirname(__FILE__).'/inc/tpl/footer.php';
