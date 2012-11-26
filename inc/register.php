ffc6b67545247c6d43923f7f7d2c38cd465c23ea
Rémy <remy@colorz.fr>
2012-11-26 14:38:43 +0100

    spam filter more invisible + comments + description

--- inc/register.php
+++ inc/register.php
@@ -1,23 +1,28 @@
 <?php
+$data_format = "Name,E-mail,Notes\r\n";
 $splash_log = "";
 if(isset($_POST) && !empty($_POST)) {
+    // Spam filter
     if(empty($_POST['splash_email']) || $_POST['splash_email'] != "hello@thenew.fr") {
         $splash_log .= "The spam filter has detected an intrusion.";
+    // Email validate filter
     } else if(empty($_POST['splash_spam']) || !filter_var($_POST['splash_spam'], FILTER_VALIDATE_EMAIL)) {
         $splash_log .= "The email is not correct.";
     } else {
         $path = dirname(__FILE__).'/../mails/';
         $file = $path.'mails.csv';
-
+        // POST values
         $mail = $_POST['splash_spam'];
         $name = explode("@", $mail);
         $name = $name[0];
+        // Name, E-mail, Notes
         $data = array($name, $mail, date('c'));
         if(!file_exists($file)) {
             mkdir($path, 0755);
-            @file_put_contents($file, "Name,E-mail,Notes\r\n");
+            @file_put_contents($file, $data_format);
         }
         @chmod($path, 0755);
+
         @file_put_contents($file, implode(",", $data)."\r\n", FILE_APPEND | LOCK_EX);
         $splash_log .= "Thanks. Your email <em>".$mail."</em> has been registered.<br /> You will hear about us soon.";
     }
--- index.php
+++ index.php
@@ -3,10 +3,13 @@ include dirname(__FILE__).'/inc/register.php';
 include dirname(__FILE__).'/inc/tpl/header.php';
 ?>
 <header class="header">
-    <h1 class="logo">Splash sreen</h1>
+    <div class="v-wrapper">
+        <h1 class="logo">Splashsreen</h1>
+        <p class="description">Splashsreen is a PHP starter splash page. Perfect to reveal projects in their early phases and to gather emails of future users.</p>
+    </div>
 </header>
 <div class="container">
-    <div class="splash-wrapper">
+    <div class="v-wrapper">
         <form action="" method="post" class="splash-form">
             <?php if($splash_log): ?>
                 <div class="splash-form-feedback">
@@ -15,13 +18,13 @@ include dirname(__FILE__).'/inc/tpl/header.php';
             <?php endif; ?>
             <div class="box splash-email">
                 <label for="splash_email">Email</label>
-                <input type="email" name="splash_email" class="spash-email-form" value="hello@thenew.fr" required />
+                <input type="text" name="splash_email" class="spash-email-form" value="hello@thenew.fr" />
             </div>
             <div class="box splash-spam">
                 <label for="splash_spam">Leave us your email to get the latest news.</label>
                 <div class="wrap">
                     <input type="email" name="splash_spam" class="splash-spam-input trans-color" placeholder="john@doe.com" tabindex="1" required />
-                    <button type="submit" class="submit trans-all">OK</button>
+                    <button type="submit" class="submit trans-all" tabindex="2">Sign Up</button>
                 </div>
             </div>
         </form>
